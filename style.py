dd3a9121d79fb4928848c8ea35dfa9b0e6f98d70
godj0ker <godjoker2004@gmail.com>
2025-05-31 07:40:41 +0530

    Update style.py

--- style.py
+++ style.py
@@ -6,4 +6,7 @@ def print_banner():
     print(Fore.LIGHTMAGENTA_EX+banner.renderText("Sub-d")) 
     print(Fore.LIGHTCYAN_EX+"Author:GODJOKER")
     print(Fore.LIGHTCYAN_EX+"Github:https://github.com/godj0ker")
-    
+    print(Fore.LIGHTCYAN_EX+"Version:1.0")
+    print(Fore.LIGHTCYAN_EX+"Date:2024-01-01")
+    print(Fore.LIGHTCYAN_EX+"Description:Subdomain enumeration tool")
+    print(Fore.LIGHTCYAN_EX+"!!This tool is onle made for testing , educational and legal Purposes only!!")
